d355cf83b7d66aba38c2972edb9bec3548def509
Максим Алехин <Max-crf@mail.ru>
2022-02-06 15:20:02 +0700

    Добавьте файлы проекта.

---
+++ Задание 21. Многопоточность. Класс Thread/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Задание_21.Многопоточность.Класс_Thread
+{
+    internal class Program
+    {
+
+        //const int n = 10;
+        //static int[] path = new int[n] { 1, 2, 0, 5, 5, 0, 1, 2, 4, 8 };
+        static int[,] field;
+        static int width;
+        static int length;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nВведите размер поля в ширину:");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nВведите размер поля в длинну:");
+            length = Convert.ToInt32(Console.ReadLine());
+            field = new int[width, length];
+            Random random = new Random();
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+
+                    field[i, j] = random.Next(9);
+
+                }
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+            }
+
+            ThreadStart threadStart = new ThreadStart(Gardner1);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+            Gardner2();
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    Console.Write(field[ i ,  j]  +  "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        static void Gardner1()
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if (field[i, j] >= 0)
+                    {
+                        int delay = field[i, j];
+                        field[i, j] = -1;
+                        Thread.Sleep(delay);
+                        Console.ForegroundColor = ConsoleColor.Blue;
+
+                    }
+                   
+                }
+
+            }
+
+        }
+        static void Gardner2()
+        {
+            for (int i = width - 1; i >= 0; i--)
+            {
+                for (int j = length - 1; j >= 0; j--)
+                {
+                    if (field[i, j] >= 0)
+                    {
+                        int delay = field[i, j];
+                        field[i, j] = -2;
+                        Thread.Sleep(delay);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                }
+
+            }
+
+        }
+
+        //static void Gardner1()
+        //{
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        if (path[i] >= 0)
+        //        {
+        //            int delay = path[i];
+        //            path[i] = -1;
+        //            Thread.Sleep(delay);
+        //        }
+        //    }
+
+        //}
+        //static void Gardner2()
+        //{
+        //    for (int i = n-1; i >= 0; i--)
+        //    {
+        //        if (path[i] >= 0)
+        //        {
+        //            int delay = path[i];
+        //            path[i] = -2;
+        //            Thread.Sleep(delay);
+        //        }
+        //    }
+
+        //}
+    }
+}
---
+++ Задание 21. Многопоточность. Класс Thread/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Задание 21. Многопоточность. Класс Thread")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Задание 21. Многопоточность. Класс Thread")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7cbc6d5e-2880-459d-8a51-a5d54e9e6f1c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
